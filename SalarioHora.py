0782545fe48c598ef692660c6c8913b196da6663
Rodrigo dos Santos Vargas <pigsrg@hotmail.com>
2023-07-12 16:54:01 -0300

    Subindo o projeto

---
+++ SalarioHora.py
@@ -0,0 +1,15 @@
+# Sistema que calcula o valor hora trabalhado
+
+
+salario = float(input("Informe o seu salario Bruto: "))
+dia = float(input("Informe o total de dias trabalhados na semana: "))
+horas = float(input("Informe o numero de horas trabalhadas por dia: "))
+semana =float(input("Informe o numero de semanas trabalhados no mês: "))
+
+horasSemanais = horas * dia
+horasMensais = horasSemanais * semana
+valorHora = salario / horasMensais
+print("-"*100)
+print(f"Voce trabalha {horasSemanais} horas semanais")
+print(f"Voce trabalha {horasMensais} horas mensais")
+print(f"Voce recebe R$ {valorHora} por hora trabalhada")
